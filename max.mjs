0ff35411413e53fe0165c7396c8cbda219b83b5e
Mathias Bynens <mathias@qiwi.be>
2025-09-06 13:38:12 +0200

    Add 3 achievement points for The Forbidden Build
    https://tibia.fandom.com/wiki/The_Forbidden_Build

--- max.mjs
+++ max.mjs
@@ -4,7 +4,7 @@
 import { computeBossBonuses } from './boss-points-utils.mjs';
 
 // Excluding coinciding achievements.
-export const MAX_ACHIEVEMENT_POINTS = 1_380;
+export const MAX_ACHIEVEMENT_POINTS = 1_383;
 
 export const MAX_CHARM_POINTS = 25_989;
 
