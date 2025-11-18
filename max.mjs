0155b78aaa32115b1621b4380e5f919bf5be7ef9
Mathias Bynens <mathias@qiwi.be>
2025-11-18 06:50:18 +0100

    Update maxima per Winter Update 2025

--- max.mjs
+++ max.mjs
@@ -4,11 +4,11 @@
 import { computeBossBonuses } from './boss-points-utils.mjs';
 
 // Excluding coinciding achievements.
-export const MAX_ACHIEVEMENT_POINTS = 1_388;
+export const MAX_ACHIEVEMENT_POINTS = 1_399;
 
 export const MAX_CHARM_POINTS = 26_009;
 
-export const MAX_BOSS_POINTS = 26_400;
+export const MAX_BOSS_POINTS = 26_850;
 export const MAX_BOSS_BONUSES = computeBossBonuses(MAX_BOSS_POINTS);
 
 // Characters that rook themselves can unlock a number of coinciding
