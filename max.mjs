cc6e9475dc34a13bfd5325e4f94649fcbac50c30
Mathias Bynens <mathias@qiwi.be>
2026-06-23 18:56:10 +0200

    Add Mimic achievement (1 point)

--- max.mjs
+++ max.mjs
@@ -4,7 +4,7 @@
 import { computeBossBonuses } from './boss-points-utils.mjs';
 
 // Excluding coinciding achievements.
-export const MAX_ACHIEVEMENT_POINTS = 1_422;
+export const MAX_ACHIEVEMENT_POINTS = 1_423;
 
 export const MAX_CHARM_POINTS = 26_984;
 
