03887ecf3ddd2a1547759b110759d5bef2ace98d
Mathias Bynens <mathias@qiwi.be>
2026-08-11 18:46:18 +0200

    Update achievement point calculation per 2026-08-11 update
    https://www.tibia.com/news/?subtopic=newsarchive&id=8925

--- max.mjs
+++ max.mjs
@@ -4,7 +4,7 @@
 import { computeBossBonuses } from './boss-points-utils.mjs';
 
 // Excluding coinciding achievements.
-export const MAX_ACHIEVEMENT_POINTS = 1_446;
+export const MAX_ACHIEVEMENT_POINTS = 1_494;
 
 export const MAX_REGULAR_CHARM_POINTS = 28_734;
 export const MAX_ECHO_WARDEN_CHARM_POINTS = 8_497;
@@ -15,13 +15,13 @@ export const MAX_BOSS_POINTS = 27_450;
 export const MAX_BOSS_BONUSES = computeBossBonuses(MAX_BOSS_POINTS);
 
 // Prior to the Newhaven update [1], characters could rook themselves
-// and unlock a number of coinciding achievements, resulting in some
-// additional points. Since this was feasible for new characters but
-// not feasible for characters that already existed when achievements
-// were introduced, and also because rooking is no longer possible
-// since [1], this is considered to be unfair overall.
+// and unlock a number of coinciding or otherwise special achievements,
+// resulting in some additional points. Since this was feasible for new
+// characters but not feasible for characters that already existed when
+// achievements were introduced, and also because rooking is no longer
+// possible since [1], this is considered to be unfair overall.
 //
 // [1]: https://tibia.fandom.com/wiki/Updates/15.12.c7d92c
-export const UNFAIR_ACHIEVEMENT_POINTS = 40;
+export const UNFAIR_ACHIEVEMENT_POINTS = 3;
 export const MAX_ACHIEVEMENT_POINTS_UNFAIR =
 	MAX_ACHIEVEMENT_POINTS + UNFAIR_ACHIEVEMENT_POINTS;
